21c8aea3e2f3d5c452c65f03c8e288f4dd989781
DesertRider <24532718+DesertRider@users.noreply.github.com>
2019-11-05 22:44:57 -0500

    Update Rpi-server-install.sh

--- Rpi-server-install.sh
+++ Rpi-server-install.sh
@@ -22,7 +22,6 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqz35tveThInSFi4ptaMkVKoxHrsxhUDb9xC6epDuu/3s2hvFPyuw8QOGq06tP5WmHeXgLgatzdHX5LrJ4zb425ExRlGOV85BP5tpgi1d+7bJgD94og4Fq475RjpPc5T5OKC+jYhDf/JMvID+PpDWRpPkaKUYdvmwacH95j7i1XuldUu5nKatIK3b14eWyPhmVdtAmw2C+gncTNSdZG5WM5mMfOEmbDMYcduGlUlKOB5IXp9D+E1Mg8J4gWUA6UEpqoSBjJMvSH5Vu3FlcAy0A2f0a9ckrlNsQ6EtW+8FOEFwamEoiLR8QHbV0Djya7WP7DDww4z2HjLc6mg3MTkxL serge@iMac-de-Serge.local' >> ~/.ssh/authorized_keys
 
 # --- Restrictions d'accès SSH (si ouvert sur Internet)
-
 # useradd pi-admin ...
 
 # sudo vim /etc/ssh/sshd_config
@@ -38,32 +37,40 @@ wget http://prdownloads.sourcefsudo apt-get install nfs-kernel-server -yorge.net
 sudo dpkg --install webmin_1.930_all.deb
 
 # --- Installation NFS Server
-
 sudo apt-get install nfs-kernel-server -y
 
 # --- Installation de xfs pour clé USB
 sudo apt-get install -y xfsprogs
 
-# --- Ajuster /boot/cmdline.txt (quirks)
-# ajouter
-usb-storage.quirks=152d:0578:u au début de la ligne
-
 # --- Partionnage et formattage du disque SSD
 # fdisk créer partition 1 = 16gb, partition 2 = reste
 sudo mkfs -t ext4 -L rootfs /dev/sda1
 sudo mkfs -t xfs  -L data   /dev/sda2
-sudo mkdir /data
 
-UUID=$(lsblk -o UUID /dev/sda2 | tail -n1 | awk '{print $1}')
-echo "UUID=$UUID  /data    xfs rw,relatime,attr2,inode64,noquota 0 0" | sudo tee -a /etc/fstab
-sudo mount -a
+PARTUUID1=$(lsblk -o PARTUUID /dev/sda1 | tail -n1 | awk '{print $1}')
+PARTUUID2=$(lsblk -o PARTUUID /dev/sda2 | tail -n1 | awk '{print $1}')
 
-# --- Préparation du disque /data
+# --- Identification de la partition boot sur SSD
+# --- Ajuster /boot/cmdline.txt (quirks)
+sudo cp /boot/cmdline.txt /boot/cmdline.txt.bkp
+echo "usb-storage.quirks=152d:0578:u console=serial0,115200 console=tty1 root=PARTUUID=6a0660a9-4e90-814d-8a78-977afc39391e rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait program_usb_timeout=1" | sudo tee -a /boot/cmdline.txt
 
+# --- copier / sur 1ere partition disque ssd
+sudo mount /dev/sda1 /mnt
+sudo rsync -ax / /mnt
+sudo mkdir -p /mnt/data
+
+# retirer ligne de mount de / avec SED
+sudo sed -i -e "/ \/ /d" -e "/\/data/d" /mnt/etc/fstab
+echo "PARTUUID=$PARTUUID1  /        ext4    defaults,noatime  0 1" | sudo tee -a /mnt/etc/fstab
+echo "PARTUUID=$PARTUUID2  /data    xfs     rw,relatime,attr2,inode64,noquota 0 0" | sudo tee -a /mnt/etc/fstab
+# on attends le prochain redémarrage pour monter /data
+
+# --- Préparation du disque /data et shares NFS
+sudo mkdir -p /data/backups/bidule1 /data/partages/bidule1
 echo "/data/backups/bidule1   192.168.0.253(no_root_squash,insecure,rw)" | sudo tee -a /etc/exports
 echo "/data/partages/bidule1  192.168.0.253(no_root_squash,insecure,rw)" | sudo tee -a /etc/exports
-sudo mkdir -p /data/backups/bidule1 /data/partages/bidule1
-sudo exportfs -a
+# sudo exportfs -a
 
 
 # ---
